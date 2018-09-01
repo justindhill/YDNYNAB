732c913cb6887bea4deffbb3d403d869075ef06b
Justin Hill <justin@justins-macbook-pro.local>
2018-09-01 16:46:11 -0700

    Initial Commit

---
+++ YDNYNAB/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  YDNYNAB
+//
+//  Created by Justin Hill on 9/1/18.
+//  Copyright © 2018 Justin Hill. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
